e58a0cd8ef962f02bfb1b6773137f7aaad5e82ff
Ayush Gupta <7221434+ayushgupta0610@users.noreply.github.com>
2024-08-27 18:16:59 +0530

    Making a small change

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   //   res.send("Hi, there!");
-  res.send({ hi: "there!" });
+  res.send({ hi: "Making a small change!" });
 });
 
 app.listen(PORT, () => {
